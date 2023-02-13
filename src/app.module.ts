0cf4efcacae960a1c3853f0f8f5b4ba0406a15a5
Liliia Smelova <26244983+LiliyaSm@users.noreply.github.com>
2023-02-13 14:59:24 +1100

    feat: connect typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,11 @@ import { TracksModule } from './tracks/tracks.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+dotenv.config();
 @Module({
   imports: [
     UserModule,
@@ -14,8 +18,19 @@ import { FavoritesModule } from './favorites/favorites.module';
     ArtistsModule,
     AlbumsModule,
     FavoritesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: false,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
