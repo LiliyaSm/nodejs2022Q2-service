f2b9300e68d913114d660188da71aad33b928328
Liliia Smelova <26244983+LiliyaSm@users.noreply.github.com>
2023-02-26 14:00:33 +1100

    fix: fix dto according to new tests

--- src/albums/dto/create-album.dto.ts
+++ src/albums/dto/create-album.dto.ts
@@ -1,5 +1,13 @@
+import { IsOptional, IsNotEmpty, IsString, IsNumber } from 'class-validator';
+
 export class CreateAlbumDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+  @IsNotEmpty()
+  @IsNumber()
   year: number;
+  @IsOptional()
+  @IsString()
   artistId: string | null; // refers to Artist
 }
